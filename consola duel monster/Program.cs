c9e9b6e310c86a718ec80abff62d4d4b0ae0acc1
MARTIN CORTEZ LOPEZ <I20050008@monclova.tecnm.mx>
2020-12-11 16:35:49 -0600

    Agregar archivos de proyecto.

---
+++ consola duel monster/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace consola_duel_monster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DECLARA LAS DESCRIPSIONES DE LOS VALORES
+            monster monster1 = new monster();
+            monster1.nombre = "OBELISCO ATORMENTADOR";
+            monster1.ataque = 4000;
+            monster1.defensa = 4000;
+
+            monster monster2 = new monster();
+            monster2.nombre = "DRAGON ALDO DE RA";
+            monster2.ataque = 4000;
+            monster2.defensa = 4000;
+
+            monster monster3 = new monster();
+            monster3.nombre = "SLIFER DRAGON CELESTIAL";
+            monster3.ataque = 4000;
+            monster3.defensa = 4000;
+
+            monster monster4 = new monster();
+            monster4.nombre = "DRAGON BLANCO DE OJOS AZULES";
+            monster4.ataque = 3000;
+            monster4.defensa = 2500;
+
+            monster monster5 = new monster();
+            monster5.nombre = "MAGO OSCURO";
+            monster5.ataque = 2500;
+            monster5.defensa = 2100;
+
+            //DECLARA E INSERTA LOS VALORES EN LA CONSOLA
+            Console.WriteLine($"el es {monster1.nombre}");
+            Console.WriteLine($"tiene {monster1.ataque} de ataque" + $" y {monster1.defensa} de defensa");
+
+            Console.WriteLine($"el es {monster2.nombre}");
+            Console.WriteLine($"tiene {monster2.ataque} de ataque" + $" y {monster2.defensa} de defensa");
+
+            Console.WriteLine($"el es {monster3.nombre}");
+            Console.WriteLine($"tiene {monster3.ataque} de ataque" + $" y {monster3.defensa} de defensa");
+
+            Console.WriteLine($"el es {monster4.nombre}");
+            Console.WriteLine($"tiene {monster4.ataque} de ataque" + $" y {monster4.defensa} de defensa");
+
+            Console.WriteLine($"el es {monster5.nombre}");
+            Console.WriteLine($"tiene {monster5.ataque} de ataque" + $" y {monster5.defensa} de defensa");
+
+        }
+    }
+}
---
+++ consola duel monster/monster.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace consola_duel_monster
+{
+    public class monster
+    {
+        
+        public string nombre;
+        public int ataque;
+        public int defensa;
+        public monster()
+        {
+
+        }
+    }
+}
